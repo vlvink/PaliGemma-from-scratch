1bde4b77ebe3f6be6eb29de0c25169b99abb95ff
Vladislav Vinokurov <vinokurov.va@phystech.edu>
2025-01-19 13:18:24 +0300

    finished gemma and inference code

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -1,10 +1,8 @@
 import torch
 from torch import nn, Tensor
 from typing import Optional, Tuple, List, Any
-from torch.nn import CrossEntropyLoss
 import math
 from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
-from processing_paligemma import normalize
 
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     batch, num_key_value_heads, slen, head_dim = hidden_states.shape
@@ -12,6 +10,20 @@ def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
         return hidden_states
     hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+
+def rotate_half(x):
+    # Build the [-x2, x1, -x4, x3, ...] tensor of the sin part of the positional encoding
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
 
 
 class KVCache():
@@ -137,6 +149,39 @@ class GemmaMLP(nn.Module):
 
     def forward(self, x):
         return self.down_proj(nn.functional.gelu(self.gate_proj(x), approximate="tanh") * self.up_proj(x))
+
+
+class GemmaRotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+        self.dim = dim
+        self.max_position_embeddings = max_position_embeddings
+        self.base = base
+
+        # Calculate the theta according to the formula: theta_i = base^(2i / dim)
+        inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float() / self.dim))
+        self.register_buffer('inv_freq', tensor=inv_freq, persistent=False)
+
+    @torch.no_grad()
+    def forward(self, x, position_ids, seq_len=None):
+        # x: [Batch_size, Num_Attention_heads, Seq_len, Head_size]
+        self.inv_freq = self.inv_freq.to(x.device)
+        # inv_freq_expanded: [Batch_size, Head_dim // 2, 1]
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, -1)
+        # position_ids_expanded: [Batch_size, 1, Seq_len]
+        position_ids_expanded = position_ids[:, None, :].float()
+        device_type = x.device.type
+        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):
+            # Multyply each theta by the position (which is the argument of the sin and cos functions)
+            # freqs: [Batch_size, Head_dim // 2, 1] @ [Batch_size, 1, Seq_len] -> [Batch_size, Seq_len, Head_dim // 2]
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            # emb: [Batch_size, Seq_len, Head_dim]
+            emb = torch.cat((freqs, freqs), dim=-1)
+            # cos, sin: [Batch_size, Seq_len, Head_dim]
+            cos = emb.cos()
+            sin = emb.sin()
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
 
 
 class GemmaAttention(nn.Module):
